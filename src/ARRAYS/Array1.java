9fe46372f01ee2f39ed8a0199d63483a4b51098d
lokeshkusumanchi <108117802+lokeshkusumanchi@users.noreply.github.com>
2022-06-30 12:06:27 +0530

    Updated array
    Modified

--- src/ARRAYS/Array1.java
+++ src/ARRAYS/Array1.java
@@ -6,8 +6,8 @@ public class Array1 {
 		// TODO Auto-generated method stub
 		//declares an array of integers
 		int[]x;
-		x=new int[] {22,54,33,66,63,44,12};
-		//displaying array elements using loop
+		x=new int[] {43,44,12,67,87,65};
+		//displaying array element using loop
 		for(int i=0;i<x.length;i++)
 		{
 			System.out.println("element at index" +i+":"+x[i]);
